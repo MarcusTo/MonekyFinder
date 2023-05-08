dbeae6ee34be990f6bdbcc75679cdba976692d5c
MarcusTo <toman.markus12@gmail.com>
2023-05-08 13:56:28 +0300

    Added monkey model strings, changes to mainpage

--- MonekyFinder/Model/Monkey.cs
+++ MonekyFinder/Model/Monkey.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Model
 {
-    internal class Monkey
+    public class Monkey
     {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        public string Image { get; set; }
+        public string Population { get; set; }
+
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+
     }
 }
