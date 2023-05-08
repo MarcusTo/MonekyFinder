5287904452a573b62cc9f66939f6fe86844b13de
MarcusTo <toman.markus12@gmail.com>
2023-05-08 15:00:51 +0300

    Changes to MonkeyService

--- MonekyFinder/Services/MonkeyService.cs
+++ MonekyFinder/Services/MonkeyService.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Net.Http.Json;
 using System.Text;
 
-namespace Services
+namespace Services;
+public class MonkeyService
 {
-    internal class MonkeyService
-    {
-    }
+        HttpClient httpClient;
+
+        List<Monkey> monkeyList = new();
+        
+        public async Task<List<Monkey>> GetMonkeys()
+        {
+            if(monkeyList?.Count > 0)
+                return monkeyList;
+
+            var url = "https://montemagno.com/monkeys.json";
+
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+            }
+
+            return monkeyList;
+        }
+
 }
+
