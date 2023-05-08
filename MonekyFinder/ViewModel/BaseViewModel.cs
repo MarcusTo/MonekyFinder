0d4e7f9387a8721d884334a6d284379870712ddf
MarcusTo <toman.markus12@gmail.com>
2023-05-08 14:31:33 +0300

    Changees to BaseViewModel, added GlobalUsings,

--- MonekyFinder/ViewModel/BaseViewModel.cs
+++ MonekyFinder/ViewModel/BaseViewModel.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
-namespace ViewModel
+namespace MonkeyFinder.ViewModel;
+public class BaseViewModel : INotifyPropertyChanged
 {
-    public class BaseViewModel
+    bool isBusy;
+    string title;
+
+    public bool IsBusy
     {
+        get => isBusy;
+        set
+        {
+            if (isBusy == value)
+                return;
+
+            isBusy = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
