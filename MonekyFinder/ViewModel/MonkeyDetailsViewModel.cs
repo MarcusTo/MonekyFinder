5793cb732dd8eecd3f64f86d79506ab353345dfe
MarcusTo <toman.markus12@gmail.com>
2023-05-22 14:08:49 +0300

    Changes to MainPage, MonkeyViewModel and Mainpage

--- MonekyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ MonekyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace MonkeyFinder.ViewModel
+[QueryProperty("Monkey","Monkey")]
+
+namespace MonkeyFinder.ViewModel;
+public partial class MonkeyDetailsViewModel : BaseViewModel
 {
-    public partial class MonkeyDetailsViewModel : BaseViewModel
+    public MonkeyDetailsViewModel()
     {
+        this.monkey;
     }
+    [ObservableObject]
+    Monkey monkey;
+
 }
+
