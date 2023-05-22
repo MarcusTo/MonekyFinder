0969413ed12609d2dd54b80e02ed944e47c5f93b
MarcusTo <toman.markus12@gmail.com>
2023-05-22 13:46:21 +0300

    MonkeyViewModel corrected

--- MonekyFinder/ViewModel/BaseViewModel.cs
+++ MonekyFinder/ViewModel/BaseViewModel.cs
@@ -9,6 +9,11 @@ namespace MonkeyFinder.ViewModel;
 
 public partial class BaseViewModel : ObservableObject
 {
+    public BaseViewModel()
+    {
+        
+    }
+    
     [ObservableProperty]
     [NotifyPropertyChangedFor(nameof(IsNotBusy))]
     bool isBusy;
--- MonekyFinder/ViewModel/MonkeysViewModel.cs
+++ MonekyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,39 +1,21 @@
-﻿using Model;
+﻿using IntelliJ.Lang.Annotations;
+using Model;
+using MonkeyFinder.Services;
 using Services;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using static Android.Icu.Text.CaseMap;
 
 namespace MonkeyFinder.ViewModel;
+
 public partial class MonkeysViewModel : BaseViewModel
 {
-    public ObservableCollection<Monkey> Monkeys { get; } = new();
-    MonkeyService monkeyService;
-    IConnectivity connectivity;
-    IGeolocation geolocation;
-    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
+    MonkeyService _monkeyService;
+    public MonkeysViewModel(MonkeyService monkeyService)
     {
         Title = "Monkey Finder";
-        this.monkeyService = monkeyService;
-        this.connectivity = connectivity;
-        this.geolocation = geolocation;
+        _monkeyService = monkeyService;
+        //GetMonkeysCommand = new Command(async () => await GetMonkeysAsync());
+        //GetMonkeysCommand. // Toolkit handles this below under [RelayCommand]
     }
-
-    [RelayCommand]
-    async Task GoToDetails(Monkey monkey)
-    {
-        if (monkey == null)
-            return;
-
-        await Shell.Current.GoToAsync(nameof(DetailsPage), true, new Dictionary<string, object>
-        {
-            {"Monkey", monkey }
-        });
-    }
-
-    [ObservableProperty]
-    bool isRefreshing;
-
     [RelayCommand]
     async Task GetMonkeysAsync()
     {
@@ -42,68 +24,60 @@ public partial class MonkeysViewModel : BaseViewModel
 
         try
         {
-            if (connectivity.NetworkAccess != NetworkAccess.Internet)
-            {
-                await Shell.Current.DisplayAlert("No connectivity!",
-                    $"Please check internet and try again.", "OK");
-                return;
-            }
-
             IsBusy = true;
-            var monkeys = await monkeyService.GetMonkeys();
-
+            var monkeys = await _monkeyService.GetMonkeys();
             if (Monkeys.Count != 0)
                 Monkeys.Clear();
 
             foreach (var monkey in monkeys)
                 Monkeys.Add(monkey);
-
         }
         catch (Exception ex)
         {
-            Debug.WriteLine($"Unable to get monkeys: {ex.Message}");
-            await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+            Debug.WriteLine(ex);
+            //await App.Current
+            await Shell.Current.DisplayAlert("Error!", $"Unable to get monkeys: {ex.Message}", "OK");
         }
         finally
         {
             IsBusy = false;
-            IsRefreshing = false;
         }
 
     }
-
     [RelayCommand]
-    async Task GetClosestMonkey()
+    void DeleteMonkeys()
     {
-        if (IsBusy || Monkeys.Count == 0)
+        if (IsBusy)
             return;
 
         try
         {
-            // Get cached location, else get real location.
-            var location = await geolocation.GetLastKnownLocationAsync();
-            if (location == null)
-            {
-                location = await geolocation.GetLocationAsync(new GeolocationRequest
-                {
-                    DesiredAccuracy = GeolocationAccuracy.Medium,
-                    Timeout = TimeSpan.FromSeconds(30)
-                });
-            }
-
-            // Find closest monkey to us
-            var first = Monkeys.OrderBy(m => location.CalculateDistance(
-                new Location(m.Latitude, m.Longitude), DistanceUnits.Miles))
-                .FirstOrDefault();
-
-            await Shell.Current.DisplayAlert("", first.Name + " " +
-                first.Location, "OK");
-
+            IsBusy = true;
+            Monkeys.Clear();
         }
         catch (Exception ex)
         {
-            Debug.WriteLine($"Unable to query location: {ex.Message}");
-            await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+            Debug.WriteLine(ex);
+            //await App.Current
+            Shell.Current.DisplayAlert("Error!", $"Unable to delete monkeys: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+        [RelayCommand]
+        async Task GoToDetailsAsync(Monkey monkey)
+        {
+            if (monkey is null)
+                return;
+
+            // await Shell.Current.GoToAsync($"{nameof(DetailsPage)}?id={monkey.Name}");
+            await Shell.Current.GoToAsync($"{nameof(DetailsPage)}", true, new Dictionary<string, object>
+                {
+                    {"Monkey", monkey}
+                });
         }
     }
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+
 }
